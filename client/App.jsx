f2acfc36b5dfcb60c9093b676ad8a1c9dc6a9ed9
JessicaWildermuth <jwildermuth536@gmail.com>
2020-12-10 10:57:41 -0800

    Create quantity/total update functions

--- client/App.jsx
+++ client/App.jsx
@@ -1,17 +1,50 @@
 import React from 'react';
+import ProductInfo from './ProductInfo';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      firstName: '',
+      lastName: '',
+      email: '',
+      street1: '',
+      street2: '',
+      city: '',
+      state: '',
+      zip: '',
+      quantity: '1',
+      total: '49.99',
+      ccNum: '',
+      exp: '',
     };
+    this.updateQuantity = this.updateQuantity.bind(this);
+    this.updateTotal = this.updateTotal.bind(this);
+  }
+
+  updateQuantity(event) {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    }, this.updateTotal);
+  }
+
+  updateTotal() {
+    const { quantity } = this.state;
+    const basePrice = 49.99;
+
+    const total = basePrice * quantity;
+
+    this.setState({
+      total,
+    });
   }
 
   render() {
+    const { quantity, total } = this.state;
     return (
       <div>
-        Basic React Application
+        <ProductInfo quantity={quantity} total={total} updateQuantity={this.updateQuantity} />
       </div>
     );
   }
