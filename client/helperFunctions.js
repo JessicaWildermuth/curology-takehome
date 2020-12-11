a73654b26867737cf0f989cdc580e37f546f1b86
JessicaWildermuth <jwildermuth536@gmail.com>
2020-12-11 00:00:03 -0800

    Add credit card expiration validation functions

--- client/helperFunctions.js
+++ client/helperFunctions.js
@@ -17,4 +17,40 @@ const formatCreditCard = (ccNumber) => {
   return formatedCC;
 };
 
-export default { formatCreditCard };
+const getMinValidExpiriation = () => {
+  const currentDate = new Date();
+
+  const currentMonth = currentDate.getMonth() + 1;
+
+  const currentYear = Number(currentDate.getFullYear().toString().substring(2));
+
+  return [currentMonth, currentYear];
+};
+
+const validateExpiration = (creditCardExp) => {
+  const minValidExpiration = getMinValidExpiriation();
+
+  const minValidMonth = minValidExpiration[0];
+  const minValidYear = minValidExpiration[1];
+
+  const creditCardMonth = Number(creditCardExp.substring(0, 2));
+
+  const creditCardYear = Number(creditCardExp.substring(3));
+
+  if (minValidYear === creditCardYear && minValidMonth > creditCardMonth) {
+    return false;
+  }
+
+  const isCorrectLength = creditCardExp.length === 5;
+  const hasSlash = creditCardExp[2] === '/';
+  const isValidMonth = creditCardMonth > 0 && creditCardMonth < 13;
+  const isValidYear = creditCardYear >= minValidYear;
+
+  if (isCorrectLength && hasSlash && isValidMonth && isValidYear) {
+    return true;
+  }
+
+  return false;
+};
+
+export default { formatCreditCard, validateExpiration };
