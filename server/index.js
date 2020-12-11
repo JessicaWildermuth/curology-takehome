50971d41ae5935f46f1f1afc854a8dbe036d296f
JessicaWildermuth <jwildermuth536@gmail.com>
2020-12-11 02:45:11 -0800

    Add skeleton api routes

--- server/index.js
+++ server/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 8080;
 
 app.use(express.static('public'));
+
+app.use(bodyParser.json());
+
+app.post('/api/magic', (req, res) => {
+  console.log(req.body);
+  res.status(201).send('UID');
+});
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
