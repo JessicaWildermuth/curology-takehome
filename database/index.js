80a158704fa3c22a2eee40ec24e6debbe76d4a52
JessicaWildermuth <jwildermuth536@gmail.com>
2020-12-11 07:41:34 -0800

    Add routes and functions for db queries

--- database/index.js
+++ database/index.js
@@ -3,25 +3,36 @@ const config = require('./config.js');
 
 const pool = new Pool(config);
 
-const checkForExistingCustomer = (customerInfo, callback) => {
-  const { firstName, lastName } = customerInfo;
-  const names = [firstName, lastName];
-  const query = 'SELECT * FROM customers WHERE first_name = $1 AND last_name = $2';
-  pool.query(query, names, (error, customer) => {
+const checkOrders = (referenceNumber, callback) => {
+  const query = 'SELECT customer_ref FROM orders WHERE customer_ref = $1';
+  pool.query(query, referenceNumber, (error, orders) => {
     if (error) {
-      console.log(error, 'existingCustomer')
       callback(error);
     } else {
-      callback(null, customer.rows);
+      callback(null, orders.rows);
+    }
+  });
+};
+
+const checkForExistingCustomer = (customerInfo, callback) => {
+  const {
+    firstName, lastName, street1, street2, city, state, zip,
+  } = customerInfo;
+  const customer = [firstName, lastName, street1, street2, city, state, zip];
+  const query = 'SELECT customer_id FROM customers WHERE first_name = $1 AND last_name = $2 AND street1 = $3 AND street2 = $4 AND city = $5 AND states = $6 AND zip = $7';
+  pool.query(query, customer, (error, customers) => {
+    if (error) {
+      callback(error);
+    } else {
+      callback(null, customers.rows);
     }
   });
 };
 
 const createCustomerInfo = (customerInformation, callback) => {
-  const query = 'INSERT INTO customers (first_name, last_name, email, phone, street1, street2, city, states, zip, ccNum, expiration) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) RETURNING ID';
+  const query = 'INSERT INTO customers (first_name, last_name, email, phone, street1, street2, city, states, zip, ccNum, expiration) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) RETURNING customer_id';
   pool.query(query, customerInformation, (error, id) => {
     if (error) {
-      console.log(error, 'createCustomer');
       callback(error);
     } else {
       callback(null, id.rows);
@@ -30,12 +41,57 @@ const createCustomerInfo = (customerInformation, callback) => {
 };
 
 const createOrder = (orderInformation, callback) => {
-  const query = 'INSERT INTO orders (customerID, total, quantity, fulfilled) VALUES ($1, $2, $3, $4)';
+  console.log(orderInformation);
+  const query = 'INSERT INTO orders (customer_ref, total, quantity, fulfilled) VALUES ($1, $2, $3, $4) RETURNING id';
   pool.query(query, orderInformation, (error, orderId) => {
     if (error) {
       callback(error);
     } else {
-      callback(null, orderId.rows.id);
+      callback(null, orderId.rows);
+    }
+  });
+};
+
+const deleteOrder = (orderId, callback) => {
+  const query = 'DELETE FROM orders WHERE id = $1';
+  pool.query(query, orderId, (error, success) => {
+    if (error) {
+      callback(error);
+    } else {
+      callback(null, success);
+    }
+  });
+};
+
+const getAllOrders = (callback) => {
+  const query = 'SELECT * FROM orders LEFT JOIN customers ON customer_ref = customer_id';
+  pool.query(query, (error, orders) => {
+    if (error) {
+      callback(error);
+    } else {
+      callback(null, orders.rows);
+    }
+  });
+};
+
+const getOrderById = (id, callback) => {
+  const query = 'SELECT * FROM orders WHERE id = $1 LEFT JOIN customers ON customer_ref = customer_id';
+  pool.query(query, id, (error, order) => {
+    if (error) {
+      callback(error);
+    } else {
+      callback(null, order.rows);
+    }
+  });
+};
+
+const updateOrderFulfillment = (orderUpdate, callback) => {
+  const query = 'UPDATE orders SET fulfillment = $2 WHERE id = $1';
+  pool.query(query, orderUpdate, (error, success) => {
+    if (error) {
+      callback(error);
+    } else {
+      callback(null, success);
     }
   });
 };
@@ -44,4 +100,9 @@ module.exports = {
   checkForExistingCustomer,
   createOrder,
   createCustomerInfo,
+  deleteOrder,
+  checkOrders,
+  getAllOrders,
+  getOrderById,
+  updateOrderFulfillment,
 };
