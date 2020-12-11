cb5664e6d509b5a623caa3608b701000669e558e
JessicaWildermuth <jwildermuth536@gmail.com>
2020-12-11 01:22:03 -0800

    Add phone number input

--- client/BillingInfo.jsx
+++ client/BillingInfo.jsx
@@ -4,13 +4,13 @@ import PropTypes from 'prop-types';
 const BillingInfo = ({
   firstName, lastName, email, street1, street2, city,
   state, zip, ccNum, exp, updateBilling, submitOrder,
+  phone,
 }) => (
   <div>
     <h1>Billing | Customer Information</h1>
     <form onSubmit={submitOrder}>
       <input type="text" value={firstName} name="firstName" onChange={updateBilling} placeholder="First Name" required />
       <input type="text" value={lastName} name="lastName" onChange={updateBilling} placeholder="Last Name" required />
-      <input type="email" value={email} name="email" onChange={updateBilling} placeholder="Email" required />
       <input type="text" value={street1} name="street1" onChange={updateBilling} placeholder="Street 1" required />
       <input type="text" value={street2} name="street2" onChange={updateBilling} placeholder="Street 2" />
       <input type="text" value={city} name="city" onChange={updateBilling} placeholder="City" required />
@@ -69,6 +69,8 @@ const BillingInfo = ({
         <option value="WY">Wyoming</option>
       </select>
       <input type="text" value={zip} name="zip" onChange={updateBilling} placeholder="zip" required />
+      <input type="email" value={email} name="email" onChange={updateBilling} placeholder="Email" required />
+      <input type="tel" value={phone} name="phone" onChange={updateBilling} placeholder="Phone Number" required />
       <input type="text" minLength="15" value={ccNum} name="ccNum" onChange={updateBilling} placeholder="Credit Card Number" required />
       <input type="month" value={exp} name="exp" onChange={updateBilling} placeholder="Expiration Date" required />
       <input type="submit" value="Complete Order" />
@@ -85,6 +87,7 @@ BillingInfo.propTypes = {
   city: PropTypes.string.isRequired,
   state: PropTypes.string.isRequired,
   zip: PropTypes.string.isRequired,
+  phone: PropTypes.string.isRequired,
   ccNum: PropTypes.string.isRequired,
   exp: PropTypes.string.isRequired,
   updateBilling: PropTypes.func.isRequired,
