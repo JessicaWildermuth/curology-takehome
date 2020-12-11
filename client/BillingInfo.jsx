c68dbfd01aa914a5742846da0f07066f35c60181
JessicaWildermuth <jwildermuth536@gmail.com>
2020-12-10 19:59:52 -0800

    Add minLength for CC validation

--- client/BillingInfo.jsx
+++ client/BillingInfo.jsx
@@ -2,21 +2,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const BillingInfo = ({
-  firstName, lastName, email, street1, street2, city, state, zip, ccNum, exp, updateBilling,
+  firstName, lastName, email, street1, street2, city,
+  state, zip, ccNum, exp, updateBilling, submitOrder,
 }) => (
   <div>
     <h1>Billing | Customer Information</h1>
-    <form>
-      <input type="text" value={firstName} name="firstName" onChange={updateBilling} placeholder="First Name" />
-      <input type="text" value={lastName} name="lastName" onChange={updateBilling} placeholder="Last Name" />
-      <input type="email" value={email} name="email" onChange={updateBilling} placeholder="Email" />
-      <input type="text" value={street1} name="street1" onChange={updateBilling} placeholder="Street 1" />
+    <form onSubmit={submitOrder}>
+      <input type="text" value={firstName} name="firstName" onChange={updateBilling} placeholder="First Name" required />
+      <input type="text" value={lastName} name="lastName" onChange={updateBilling} placeholder="Last Name" required />
+      <input type="email" value={email} name="email" onChange={updateBilling} placeholder="Email" required />
+      <input type="text" value={street1} name="street1" onChange={updateBilling} placeholder="Street 1" required />
       <input type="text" value={street2} name="street2" onChange={updateBilling} placeholder="Street 2" />
-      <input type="text" value={city} name="city" onChange={updateBilling} placeholder="City" />
-      <input type="text" value={state} name="state" onChange={updateBilling} placeholder="State" />
-      <input type="text" value={zip} name="zip" onChange={updateBilling} placeholder="zip" />
-      <input type="text" value={ccNum} name="ccNum" onChange={updateBilling} placeholder="Credit Card Number" />
-      <input type="month" value={exp} name="exp" onChange={updateBilling} placeholder="Expiration Date" />
+      <input type="text" value={city} name="city" onChange={updateBilling} placeholder="City" required />
+      <input type="text" value={state} name="state" onChange={updateBilling} placeholder="State" required />
+      <input type="text" value={zip} name="zip" onChange={updateBilling} placeholder="zip" required />
+      <input type="text" minLength="15" value={ccNum} name="ccNum" onChange={updateBilling} placeholder="Credit Card Number" required />
+      <input type="month" value={exp} name="exp" onChange={updateBilling} placeholder="Expiration Date" required />
+      <input type="submit" value="Complete Order" />
     </form>
   </div>
 );
@@ -33,6 +35,7 @@ BillingInfo.propTypes = {
   ccNum: PropTypes.string.isRequired,
   exp: PropTypes.string.isRequired,
   updateBilling: PropTypes.func.isRequired,
+  submitOrder: PropTypes.func.isRequired,
 };
 
 export default BillingInfo;
